ae939c7019e730bc4dc7cdbf0e333db8cc95d085
Bhuwan Bhatt <bhattbhuwan1999@gmail.com>
2025-12-13 13:42:39 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,208 @@
+
+require("dotenv").config();
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const User = require("./models/user.model");
+const Note = require("./models/note.model");
+
+
+
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+const jwt = require("jsonwebtoken")
+const { authenticateToken } = require("./utilities")
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*"
+    })
+);
+app.get("/", (req, res) => {
+    res.json({ data: " hello " });
+});
+
+//create account 
+app.post("/create-account", async (req, res) => {
+
+    const { fullName, email, password } = req.body;
+
+    if (!fullName) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Full Name is required " })
+    }
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" })
+    }
+    if (!password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Password is required " })
+    }
+    const isUser = await User.findOne({ email: email });
+    if (isUser) {
+        return res.json({
+            error: true,
+            message: "User already exist",
+
+        })
+    }
+    const newUser = new User({
+        fullName,
+        email,
+        password
+    })
+    await newUser.save();
+    const accessToken = jwt.sign(
+        { user: newUser },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "36000m" }
+    );
+    return res.json({
+        error: false,
+        user: newUser,
+        accessToken,
+        message: "Resistration Successful",
+    });
+})
+
+// login
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
+        return res.status(400).json({
+            message: "Email is required"
+        });
+    }
+    if (!password) {
+        return res.status(400).json({ message: "password is required " })
+    }
+    const userInfo = await User.findOne({
+        email: email
+    });
+    if (!userInfo) {
+        return res.status(400).json({ message: "User not found " });
+    }
+    if (userInfo.email === email && userInfo.password === password) {
+        const user = { user: userInfo };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m"
+        });
+        return res.json({
+            error: false,
+            message: " login Successful",
+            email,
+            accessToken,
+        });
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Invelid Credentials",
+        });
+    }
+})
+
+//add notes 
+app.post("/add-note", authenticateToken, async (req, res) => {
+    console.log("REQ.USER in /add-note:", req.user);
+    const { title, content, tags } = req.body;
+    const user = req.user;
+
+    if (!title) {
+        return res.status(400).json({ error: true, message: "Title is required" })
+    }
+    if (!content) {
+        return res
+            .status(400)
+            .json({ error: true, message: "content is required" })
+    }
+    try {
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: "693a99c78d2fdfb04c98007"
+            // userId: user._id,
+        });
+        await note.save();
+        return res.json({
+            error: false,
+            note,
+            message: "Note added successfuly",
+        });
+    } catch (error) {
+        console.error("ADD NOTE ERROR:", error)
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        });
+    }
+})
+
+
+//edit note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const  user  = req.user;
+    //const { _id: userId } = req.user;
+
+    if (!title  && !content  && !tags) {
+        return res
+            .status(400)
+            .json({ error: true, message: "no changes provided" });
+    }
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+        if (!note) {
+            return res.status(400).json({ error: true, message: "Note not found " });
+        }
+        if (title ) note.title = title;
+        if (content ) note.content = content;
+        if (tags ) note.tags = tags;
+        if (isPinned ) note.isPinned = isPinned;
+
+        await note.save();
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server Error",
+        });
+    }
+});
+
+//get all notes
+
+app.get("/get-all-notes/", authenticateToken, async (req, res) => {
+
+    const {user}=req.user;
+    try{
+        const notes=await Note.find({userId:user._id}).sort({isPinned:-1});
+        return res.json({
+            error:false,
+            notes,
+            message:"All notes retrieved successfully " ,
+        });
+
+    }catch(error){
+        return res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+})
+
+
+
+app.listen(8000);
+module.exports = app;
---
+++ frontend/notes-app/src/utils/helper.js
@@ -0,0 +1,21 @@
+
+ 
+export const validateEmail=(email)=>{
+    const regex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(email )
+}
+
+export const getInitials = (name) => {
+  if (!name) return "";
+
+  // Trim spaces and split by one or more spaces
+  const words = name.trim().split(/\s+/);
+  let initials = "";
+
+  // Loop for up to 2 words (or 1 if only one word)
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0]; 
+  }
+
+  return initials.toUpperCase();
+};
