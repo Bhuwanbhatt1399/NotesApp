ea81c2e9321d76bb3485a94fc4c9ee0b39cb486b
Bhuwan Bhatt <bhattbhuwan1999@gmail.com>
2026-01-14 21:54:17 +0530

    Base url update

--- frontend/notes-app/src/utils/constant.js
+++ frontend/notes-app/src/utils/constant.js
@@ -2,4 +2,4 @@
 
 // In production, there's no localhost so we have to make this dynamic
 
- export const BASE_URL = "http://localhost:8000"
+ export const BASE_URL = "https://notesapp-qmkf.onrender.com"
